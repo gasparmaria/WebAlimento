f2fae18d981244333683e8f558c055724027006b
Maria Eduarda Gaspar <73003909+gasparmaria@users.noreply.github.com>
2021-06-15 23:02:00 -0300

    Add project files.

---
+++ WebAlimento/Controllers/AlimentoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAlimento.Models;
+
+namespace WebAlimento.Controllers
+{
+    public class AlimentoController : Controller
+    {
+        // GET: Alimento
+        public ActionResult Index()
+        {
+            var alimento = new Alimento();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Alimento alimento)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", alimento);
+            }
+            return View(alimento);
+        }
+        public ActionResult Resultado(Alimento alimento)
+        {
+            return View(alimento);
+        }
+        public ActionResult CodigoUnico(string codigo)
+        {
+            var bdAlimento = new Collection<string>
+            {
+                "1",
+                "2"
+            };
+            return Json(bdAlimento.All(x => x.ToLower() != codigo.ToLower()), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WebAlimento/Models/Alimento.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace WebAlimento.Models
+{
+    public class Alimento
+    {
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        [Remote("CodigoUnico", "alimento", ErrorMessage = "Código existente.")]
+        public int Codigo { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        public string Nome { get; set; }
+
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Insira de 5 a 50 caracteres.")]
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        public string Descricao { get; set; }
+
+        [Range(1.00, 100.00, ErrorMessage = "Insira valor entre 1 e 100.")]
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        public string Peso { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        public string Categoria { get; set; }
+
+        [Range(1.00, 100.00, ErrorMessage = "Insira valor entre 1 e 100.")]
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        public string ValorUnit { get; set; }
+    }
+}
